98a98f6ed1c770ad7034ce7d43e292a279e7cde3
Khizir Farrukh <khizirfarrukh@outlook.com>
2023-01-30 11:12:15 +0500

    Replaced function with inline lambda expression to calculate total amount

--- models/GetCartList.js
+++ models/GetCartList.js
@@ -1,20 +1,12 @@
 const mysql = require('mysql');
-function GetTotalAmount(SQL_Result, callback) {
-    var TotalAmount = 0
-    for(let i=0;i<SQL_Result.length;i+=1) {
-        TotalAmount += SQL_Result[i].Quantity * SQL_Result[i].ProductPrice;
-    }
-    callback(TotalAmount);
-}
 function ExecuteQuery(PatientID, con, callback) {
     var sql = "SELECT prod.ProductID, prod.ProductName, cart.Quantity, prod.ProductPrice FROM products prod JOIN patientproductscart cart on prod.ProductID = cart.ProductID where cart.PatientID = " + PatientID + ";";
     console.log(sql)
     con.query(sql, function (err, result) {
         if (err) throw err;
-        GetTotalAmount(result, function(TotalAmount){
-            console.log("Total Amount : " + TotalAmount);
-            callback(result,TotalAmount);
-        });
+        const TotalAmount = result.reduce((total, cartItem) => total + (cartItem.ProductPrice * cartItem.Quantity), 0);
+        console.log("Total Amount : " + TotalAmount);
+        callback(result,TotalAmount);
         
     });
 }
