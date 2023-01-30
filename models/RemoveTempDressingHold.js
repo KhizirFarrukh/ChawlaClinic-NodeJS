25ad17035a968069094893241b7b09f37cceabfe
Khizir Farrukh <khizirfarrukh@outlook.com>
2023-01-30 11:13:14 +0500

    Removing of temp dressing record will now be done by TempID instead of PatientID

--- models/RemoveTempDressingHold.js
+++ models/RemoveTempDressingHold.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
-function ExecuteQuery(PatientID, con, callback) {
-  var removetempdressing_sql = "DELETE FROM patientdressingtemphold WHERE PatientID = " + PatientID;
+function ExecuteQuery(TempID, con, callback) {
+  var removetempdressing_sql = "DELETE FROM patientdressingtemphold WHERE TempID = " + TempID;
   console.log(removetempdressing_sql);
   con.query(removetempdressing_sql, function (err) {
     if (err) throw err;
