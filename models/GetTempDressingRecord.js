4f26fe932fc53b4367db753f2de2df770ef82b68
Khizir Farrukh <khizirfarrukh@outlook.com>
2023-01-30 11:15:58 +0500

    Output result will be ordered by dressing date

--- models/GetTempDressingRecord.js
+++ models/GetTempDressingRecord.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 function ExecuteQuery(id, con, callback) {
-    var sql = "SELECT * FROM `patientdressingtemphold` WHERE PatientID = " + id + ";";
+    var sql = "SELECT * FROM `patientdressingtemphold` WHERE PatientID = " + id + " ORDER BY DressingDate;";
     console.log(sql);
     con.query(sql, function (err, result) {
         if (err) throw err;
