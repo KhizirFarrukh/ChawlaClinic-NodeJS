b1e11fd7f0a6c6eb1a5b4d9097c8b64b9d5f6a3e
Khizir Farrukh <khizirfarrukh@outlook.com>
2023-02-12 14:59:52 +0500

    Modified to search by both payment ID and hash

--- models/GetPaymentIDFromHash.js
+++ models/GetPaymentIDFromHash.js
@@ -1,9 +1,14 @@
-function ExecuteQuery(Payment_HashValue, con, callback) {
-  var sql = "SELECT PaymentID FROM `patientpaymentsidentifiers` WHERE PaymentHashCode = '" + Payment_HashValue + "';";
+function ExecuteQuery(PaymentID, Payment_HashValue, con, callback) {
+  var sql = "SELECT * FROM `patientpaymentsidentifiers` WHERE ";
+  if(PaymentID !== undefined) {
+    sql += "PaymentID = " + PaymentID + ";";
+  } else if (Payment_HashValue !== undefined) {
+    sql += "PaymentHashCode = '" + Payment_HashValue + "';";
+  }
   console.log(sql);
   con.query(sql, function (err, result) {
-      if (err) throw err;
-      callback(result);
+    if (err) throw err;
+    callback(result);
   });
 }
 module.exports = { ExecuteQuery };
