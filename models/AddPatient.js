971c32e27a883877f152dd217df94a88d3a15340
Khizir Farrukh <khizirfarrukh@outlook.com>
2023-02-19 22:44:22 +0500

    Removed console outputs and only patient ID is returned

--- models/AddPatient.js
+++ models/AddPatient.js
@@ -1,68 +1,67 @@
 function ExecuteQuery(data, con, callback) {
-    var sql = "INSERT INTO patientdetails(`CaseNo`,`Type`,`PatientName`,`Age`,`Gender`,`GuardianName`,`Disease`,`Address`,`PhoneNumber`,`FirstVisit`)";
+	var sql = "INSERT INTO patientdetails(`CaseNo`,`Type`,`PatientName`,`Age`,`Gender`,`GuardianName`,`Disease`,`Address`,`PhoneNumber`,`FirstVisit`)";
 
-    var Type = "B";
-    var PatientName = data.patientname;
-    console.log("age in years:", data.ageyears);
-    console.log("age in months:", data.agemonths);
-    var Age = parseFloat(data.ageyears) + parseFloat(data.agemonths/12);
-    Age = parseFloat(Age).toFixed(3);
-    console.log("age:", Age);
-    var Gender = data.gender;
-    var GuardianName = data.guardianname;
-    var Disease = data.disease;
-    var Address = data.address;
-    var PhoneNumber = data.phonenum;
-    var FirstVisit = data.firstvisit;
-    
-    var NewCaseNo = "";
-    var CaseNoFetchParam = String((new Date(data.firstvisit)).getFullYear()%100) + Type + "-";
-    console.log("CaseNo Fetch Param:", CaseNoFetchParam);
-    NewCaseNo += CaseNoFetchParam;
-    if(data.caseno !== undefined) {
-        var sql1 = "SELECT count(`CaseNo`) AS \"CaseCount\" FROM patientdetails WHERE `CaseNo` LIKE '" + CaseNoFetchParam + "%" + data.caseno + "' AND CaseNo != 'Guest';";
-        console.log(sql1);
-        con.query(sql1, function (err, result) {
-            if (err) throw err;
-            console.log(result[0].CaseCount);
-            NewCaseNo += String(parseInt(result[0].CaseCount)).padStart(2,'0') + data.caseno;
-            console.log(NewCaseNo);
-            sql += " VALUES('" + NewCaseNo + "','" + Type + "','" + PatientName + "'," + Age + ",'" + Gender + "','" + GuardianName + "','" + Disease + "','" + Address;
-            sql += "','" + PhoneNumber + "',STR_TO_DATE('" + FirstVisit + "','%Y-%m-%d'));";
-            console.log(sql);
-            con.query(sql, function (err, result) {
-                if (err) throw err;
-                callback(result);
-            });
-        });
-    } else {
-        var sql1 = "SELECT MAX(`CaseNo`) AS \"CaseMax\" FROM patientdetails WHERE `CaseNo` LIKE '" + CaseNoFetchParam + "%' AND CaseNo != 'Guest';";
-        console.log(sql1);
-        con.query(sql1, function (err, result) {
-            if (err) throw err;
-            console.log("Max CaseNo:", result[0].CaseMax);
-            if(result[0].CaseMax == null) {
-                NewCaseNo += String(1).padStart(6,'0');
-                console.log("New CaseNo:", NewCaseNo);
-                sql += " VALUES('" + NewCaseNo + "','" + Type + "','" + PatientName + "'," + Age + ",'" + Gender + "','" + GuardianName + "','" + Disease + "','" + Address;
-                sql += "','" + PhoneNumber + "',STR_TO_DATE('" + FirstVisit + "','%Y-%m-%d'));";
-                console.log(sql);
-                con.query(sql, function (err, result) {
-                    if (err) throw err;
-                    callback(result);
-                });
-            } else {
-                NewCaseNo += String(parseInt((result[0].CaseMax).slice(4,10)) + 1).padStart(6,'0');
-                console.log("New CaseNo:", NewCaseNo);
-                sql += " VALUES('" + NewCaseNo + "','" + Type + "','" + PatientName + "'," + Age + ",'" + Gender + "','" + GuardianName + "','" + Disease + "','" + Address;
-                sql += "','" + PhoneNumber + "',STR_TO_DATE('" + FirstVisit + "','%Y-%m-%d'));";
-                console.log(sql);
-                con.query(sql, function (err, result) {
-                    if (err) throw err;
-                    callback(result);
-                });
-            }
-        });
-    }
+	var Type = "B";
+	var PatientName = data.patientname;
+	var Age = parseFloat(data.ageyears) + parseFloat(data.agemonths / 12);
+	Age = parseFloat(Age).toFixed(3);
+	var Gender = data.gender;
+	var GuardianName = data.guardianname;
+	var Disease = data.disease;
+	var Address = data.address;
+	var PhoneNumber = data.phonenum;
+	var FirstVisit = data.firstvisit;
+	var NewCaseNo = "";
+	var CaseNoFetchParam = String((new Date(data.firstvisit)).getFullYear() % 100) + Type + "-";
+	NewCaseNo += CaseNoFetchParam;
+	
+	if (data.caseno !== undefined) {
+		var sql1 = "SELECT count(`CaseNo`) AS \"CaseCount\" FROM patientdetails WHERE `CaseNo` LIKE '" + CaseNoFetchParam + "%" + data.caseno + "' AND CaseNo != 'Guest';";
+		console.log(sql1);
+		con.query(sql1, function (err, result) {
+			if (err) throw err;
+			console.log(result[0].CaseCount);
+			NewCaseNo += String(parseInt(result[0].CaseCount)).padStart(2, '0') + data.caseno;
+			console.log(NewCaseNo);
+			sql += " VALUES('" + NewCaseNo + "','" + Type + "','" + PatientName + "'," + Age + ",'" + Gender + "','" + GuardianName + "','" + Disease + "','" + Address;
+			sql += "','" + PhoneNumber + "',STR_TO_DATE('" + FirstVisit + "','%Y-%m-%d'));";
+			console.log(sql);
+			con.query(sql, function (err, result) {
+				if (err) throw err;
+				const PatientID = result.insertId;
+				callback(PatientID);
+			});
+		});
+	} else {
+		var sql1 = "SELECT MAX(`CaseNo`) AS \"CaseMax\" FROM patientdetails WHERE `CaseNo` LIKE '" + CaseNoFetchParam + "%' AND CaseNo != 'Guest';";
+		console.log(sql1);
+		con.query(sql1, function (err, result) {
+			if (err) throw err;
+			console.log("Max CaseNo:", result[0].CaseMax);
+			if (result[0].CaseMax == null) {
+				NewCaseNo += String(1).padStart(6, '0');
+				console.log("New CaseNo:", NewCaseNo);
+				sql += " VALUES('" + NewCaseNo + "','" + Type + "','" + PatientName + "'," + Age + ",'" + Gender + "','" + GuardianName + "','" + Disease + "','" + Address;
+				sql += "','" + PhoneNumber + "',STR_TO_DATE('" + FirstVisit + "','%Y-%m-%d'));";
+				console.log(sql);
+				con.query(sql, function (err, result) {
+					if (err) throw err;
+					const PatientID = result.insertId;
+					callback(PatientID);
+				});
+			} else {
+				NewCaseNo += String(parseInt((result[0].CaseMax).slice(4, 10)) + 1).padStart(6, '0');
+				console.log("New CaseNo:", NewCaseNo);
+				sql += " VALUES('" + NewCaseNo + "','" + Type + "','" + PatientName + "'," + Age + ",'" + Gender + "','" + GuardianName + "','" + Disease + "','" + Address;
+				sql += "','" + PhoneNumber + "',STR_TO_DATE('" + FirstVisit + "','%Y-%m-%d'));";
+				console.log(sql);
+				con.query(sql, function (err, result) {
+					if (err) throw err;
+					const PatientID = result.insertId;
+					callback(PatientID);
+				});
+			}
+		});
+	}
 }
 module.exports = { ExecuteQuery };
