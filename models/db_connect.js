65baaaea170140e895fa587155f2c23f0f2e36ce
Khizir Farrukh <khizirfarrukh@outlook.com>
2023-02-19 22:43:36 +0500

    db connectivity data is received from config file

--- models/db_connect.js
+++ models/db_connect.js
@@ -1,14 +1,19 @@
 const mysql = require('mysql');
+const fs = require('fs');
+
+const WebConfigData = fs.readFileSync('webconfig.json');
+const WebConfig = JSON.parse(WebConfigData);
+
 const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root"
+    host: WebConfig.database.host,
+    user: WebConfig.database.user,
+    password: WebConfig.database.password
 });
 con.connect(function(err) {
     if(err) throw err;
     console.log("connected to mysql database");
 });
-const sql = "use chawlaclinic;";
+const sql = "use " + WebConfig.database.database + ";";
 con.query(sql, function (err, result) {
     if (err) throw err;
 });
