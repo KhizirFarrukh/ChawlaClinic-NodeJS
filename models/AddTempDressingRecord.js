eed35d15f8b5b0771c892d0404c1e0f86738d792
Khizir Farrukh <khizirfarrukh@outlook.com>
2023-02-21 19:14:19 +0500

    Added option for custom dressing price

--- models/AddTempDressingRecord.js
+++ models/AddTempDressingRecord.js
@@ -1,13 +1,28 @@
 function ExecuteQuery(data, PatientID, SQL_GetPadPricing, con, callback) {
-  SQL_GetPadPricing.ExecuteQuery(data.padquantity, data.padfraction, con, function(PadPrice){
-    var padQty = parseFloat(data.padquantity) + parseFloat(eval(data.padfraction));
-    var tempdressing_sql = "INSERT INTO patientdressingtemphold(`PatientID`,`DressingDate`,`QtyOfPads`,`TotalAmount`) ";
-    tempdressing_sql += "VALUES(" + PatientID + ",STR_TO_DATE('" + data.dressingdate + "','%Y-%m-%d')," + padQty + "," + PadPrice + ");";
-    console.log(tempdressing_sql);
-    con.query(tempdressing_sql, function (err) {
-      if (err) throw err;
-      callback();
+  if(data.customprice !== "") {
+    if (/^\d+$/.test(data.customprice)) {
+			const customPrice = parseInt(data.customprice);
+      var padQty = parseFloat(data.padquantity) + parseFloat(eval(data.padfraction));
+      var tempdressing_sql = "INSERT INTO patientdressingtemphold(`PatientID`,`DressingDate`,`QtyOfPads`,`TotalAmount`) ";
+      tempdressing_sql += "VALUES(" + PatientID + ",STR_TO_DATE('" + data.dressingdate + "','%Y-%m-%d')," + padQty + "," + customPrice + ");";
+      console.log(tempdressing_sql);
+      con.query(tempdressing_sql, function (err) {
+        if (err) throw err;
+        callback();
+      });
+		}
+  } else {
+    SQL_GetPadPricing.ExecuteQuery(data.padquantity, data.padfraction, con, function(PadPrice){
+      var padQty = parseFloat(data.padquantity) + parseFloat(eval(data.padfraction));
+      var tempdressing_sql = "INSERT INTO patientdressingtemphold(`PatientID`,`DressingDate`,`QtyOfPads`,`TotalAmount`) ";
+      tempdressing_sql += "VALUES(" + PatientID + ",STR_TO_DATE('" + data.dressingdate + "','%Y-%m-%d')," + padQty + "," + PadPrice + ");";
+      console.log(tempdressing_sql);
+      con.query(tempdressing_sql, function (err) {
+        if (err) throw err;
+        callback();
+      });
     });
-  });
+  }
+  
 }
 module.exports = { ExecuteQuery };
