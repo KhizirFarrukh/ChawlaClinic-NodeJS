e00e4809636a0002cc1fb3a9a7634c78391a1183
Khizir Farrukh <khizirfarrukh@outlook.com>
2023-03-26 12:30:43 +0500

    Renamed token sql files

--- models/SQL_ResetTokenData.js
+++
@@ -1,9 +0,0 @@
-function ExecuteQuery(con, callback) {
-  var sql = "DELETE FROM patienttokennumbers;";
-  console.log(sql);
-  con.query(sql, function (err, result) {
-      if (err) throw err;
-      callback();
-  });
-}
-module.exports = { ExecuteQuery };
